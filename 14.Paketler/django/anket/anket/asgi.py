e75d1e2f4574ca7c2776d2fb4208db65d960822c
AhmetBedir <ahmet.bedir767@gmail.com>
2026-07-24 15:25:27 +0300

    Otomatik güncelleme işlemi yapıldı.

--- 14.Paketler/django/anket/anket/asgi.py
+++ 14.Paketler/django/anket/anket/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for proje_adi project.
+ASGI config for anket project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proje_adi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anket.settings')
 
 application = get_asgi_application()
--- 14.Paketler/django/anket/anket/urls.py
+++ 14.Paketler/django/anket/anket/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for proje_adi project.
+URL configuration for anket project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
