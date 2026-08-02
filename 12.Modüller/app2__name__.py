4384f5051db554d2aa92d51ecf144594c9df96d2
AhmetBedir <ahmet.bedir767@gmail.com>
2026-08-02 16:26:51 +0300

    Otomatik güncelleme işlemi yapıldı.

--- 12.Modüller/app2__name__.py
+++ 12.Modüller/app2__name__.py
@@ -14,11 +14,12 @@ def artir(sayac, kelime):
     return sayac
 
 def ekrana_bas(kelime):
-    if sayac != 0:
-        print(f"'{kelime}' kelimesinde {artir(sayac, kelime)} sesli harf var.")
-    else:
-        print(f"'{kelime}' kelimesinde sesli harf yok!")
-        
+
+    if artir(sayac, kelime) != 0:
+        print(f "'{}' kelimesinde {} sesli harf var.")
+
+    
+        # print(f"'{kelime}' kelimesinde sesli harf yok!")
 def calistir():
     kelime = kelime_sor()
     ekrana_bas(kelime)
