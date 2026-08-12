48e467e2e41c9e920ba2dbc65b3af035dbf2e430
termux <community@termux.com>
2026-08-12 13:05:01 +0300

    Otomatik Güncelleme İşlemi Yapıldı.

--- 12.Modüller/modullerin-ortak-nitelikleri.py
+++ 12.Modüller/modullerin-ortak-nitelikleri.py
@@ -1,3 +1,4 @@
+"""
 # Modüllerin Özel Nitelikleri
 import os, sys, random
 set_os = set(dir(os))
@@ -8,8 +9,9 @@ print(set_os & set_sys & set_random)
 # Bu kodlar, os, sys ve random modüllerinin kesişim kümesini, yani her üç modülde ortak olarak bulunan nitelikleri verecektir:
 # {'__doc__', '__package__', '__loader__', '__name__', '__spec__'}
 
-# Kodların yeniden kullanılabilir özellikte olması (code reusability) programcılıkta önemli bir niteliktir:
-moduller = ['os', 'sys', 'random', 'sozluk_modulu']
+
+# Kodların yeniden kullanılabilir özellikte olması (code reusability) için:
+moduller = ['os', 'sys', 'random']
 print("Modüller : ", *moduller, sep='  ')
 
 def ortak_nitelikler(moduller):
@@ -18,14 +20,18 @@ def ortak_nitelikler(moduller):
 
 print(ortak_nitelikler(moduller))
 
+"""
 ###
-moduller = ['sozluk_modulu','random']
+moduller = ['os','random']
 for modul in moduller:
-    print(dir(__import__(modul)))
+    __import__(modul)
+    
+os = __import__('os')
+print(os.getcwd())
 
 
 ### __doc__ Niteliği
-import sozluk_modulu ; print(sozluk_modulu.__doc__)
+#import uyg1.sozluk_modulu ; print(uyg1.sozluk_modulu.__doc__)
 
 
 
